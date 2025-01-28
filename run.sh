eae2157cdf4d2d4113d555fff15b34512e4ed54d
timonmerk <timon.merk95@gmail.com>
2025-01-28 18:00:59 +0100

    slurm file

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@
 #SBATCH --cpus-per-task=256
 #SBATCH -o logs/train.out
 #SBATCH -e logs/train.err
-uv run train.py 
+uv run train.py --lr 0.0001 --num_epochs 300 --d_model 64 --dim_feedforward 32 --patience 30 --use_rotary_encoding False --PATH_DATA /data/cephfs-1/home/users/merkt_c/work/dbs_foundation_model/data --path_out /data/cephfs-1/home/users/merkt_c/work/dbs_foundation_model/out
