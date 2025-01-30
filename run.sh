3b776ed88b31da6c6dbca8b36ff5b7a5d5d08559
timonmerk <timon.merk95@gmail.com>
2025-01-30 17:34:07 +0100

    add slurm files

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@
 #SBATCH -e logs/train.err
 #SBATCH -a 0-15
 
-uv run train.py --sub_idx $SLURM_ARRAY_TASK_ID --lr 0.0001 --num_epochs 300 --d_model 64 --dim_feedforward 32 --patience 30 --use_rotary_encoding False --PATH_DATA /data/cephfs-1/home/users/merkt_c/work/dbs_foundation_model/data --path_out /data/cephfs-1/home/users/merkt_c/work/dbs_foundation_model/out
+uv run train.py --sub_idx $SLURM_ARRAY_TASK_ID --pretrain_loss mae --lr 0.0001 --num_epochs 1000 --d_model 64 --dim_feedforward 32 --patience 500 --use_rotary_encoding False --PATH_DATA /data/cephfs-1/home/users/merkt_c/work/dbs_foundation_model/data --path_out /data/cephfs-1/home/users/merkt_c/work/dbs_foundation_model/out_1000
---
+++ run_lr.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#SBATCH --mem=100GB
+#SBATCH --partition=short
+#SBATCH --time=04:00:00
+#SBATCH -o logs/train_lr.out
+#SBATCH -e logs/train_lr.err
+#SBATCH -a 0-15
+
+uv run train.py --sub_idx $SLURM_ARRAY_TASK_ID --pretrain_loss mae --lr 0.001 --num_epochs 200 --d_model 64 --dim_feedforward 32 --patience 50 --use_rotary_encoding False --PATH_DATA /data/cephfs-1/home/users/merkt_c/work/dbs_foundation_model/data --path_out /data/cephfs-1/home/users/merkt_c/work/dbs_foundation_model/out_lr
