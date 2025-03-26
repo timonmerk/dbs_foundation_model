eb61269ab62fedeaa36d1c5a8ca93bc8ff2eccaf
timonmerk <timon.merk95@gmail.com>
2025-03-26 08:50:45 +0100

    add log wo hour

--- run_log.sh
+++ run_log.sh
@@ -6,4 +6,4 @@
 #SBATCH -e logs/train_log.err
 #SBATCH -a 0-15
 
-uv run train.py --sub_idx $SLURM_ARRAY_TASK_ID --pretrain_loss mae --lr 0.0001 --num_epochs 100 --d_model 64 --dim_feedforward 32 --patience 7 --use_rotary_encoding False --PATH_DATA /data/cephfs-1/home/users/merkt_c/work/dbs_foundation_model/data --path_out /data/cephfs-1/home/users/merkt_c/work/dbs_foundation_model/out_log
+uv run train.py --sub_idx $SLURM_ARRAY_TASK_ID --pretrain_loss mae --lr 0.0001 --num_epochs 100 --d_model 64 -add_hour_to_features True --dim_feedforward 32 --patience 7 --use_rotary_encoding False --PATH_DATA /data/cephfs-1/home/users/merkt_c/work/dbs_foundation_model/data --path_out /data/cephfs-1/home/users/merkt_c/work/dbs_foundation_model/out_log
